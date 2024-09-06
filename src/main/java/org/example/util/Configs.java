70f2e8e6ced95d8d87c8c5a95b9ca18725515ce6
Sumana-Jogi <sumanapta@gmail.com>
2024-09-06 18:44:46 +0530

    commit5: configs file

---
+++ src/main/java/org/example/util/Configs.java
@@ -0,0 +1,10 @@
+package org.example.util;
+
+public class Configs {
+    public static String MYSQL_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static String MYSQL_URL= "jdbc:mysql://localhost:3306/youtube";
+    public static String MYSQL_USER="root";
+    public static String MYSQL_TABLE_NAME = "user";
+    public static String CASS_KEYSPACE = "samplekeyspace";
+    public static String CASS_TABLE = "testtable";
+}
